f425e23516b10ff98514363da30f63424ab57569
Brandon Milton <brandon.milton21@gmail.com>
2016-09-15 22:11:45 -0700

    Begin basic layout of Servertrak

---
+++ common/server.py
@@ -0,0 +1,12 @@
+class Server:
+
+    def __init__(self, hostname):
+        self.hostname = hostname
+
+    def get_command_output(self, proxy, command: list)
+        """ Sends a command to the server and returns the output 
+
+        command: list<str> the command to execute, where the first element
+                 is usually the commandname and the rest are args
+        """
+        return proxy.get_command_output(self.hostname, command)
---
+++ proxies/ssh.py
@@ -0,0 +1,6 @@
+class SSHProxy:
+
+    def __init__(self):
+
+
+    def get_command_output(self, hostname, command: list):
